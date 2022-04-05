87032f80f7e181ddb431c434fec50d1e2549f1c5
itbergl <tberg644@gmail.com>
2022-04-05 14:31:16 +0800

    testing updates

---
+++ mean_reversion_moving_averages.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import time
+import multiprocessing as mp
+import numpy as np
+import matplotlib.pyplot as plt
+
+# local imports
+from backtester import engine, tester
+from backtester import API_Interface as api
+
+training_period = 20  # How far the rolling average takes into calculation
+# Number of Standard Deviations from the mean the Bollinger Bands sit
+standard_deviations = 3.5
+
+mv_av_slow = 0
+mv_av_slow_size = 10
+mv_av_fast = 0
+mv_av_fast_size = 20
+window = []
+
+scale = 10
+
+
+def stochastic_oscillator():
+    pass
+
+
+def relative_strength_index():
+    pass
+
+
+def internal_bar_strength():
+    pass
+
+
+def move_averages(value):
+    window.append(value)
+    if len(window) == mv_av_slow:
+        rm = window.pop(0)
+        slow = mv_av_slow - (rm + value)/mv_av_slow_size
+        fast = mv_av_fast - (window[-mv_av_fast_size] + value)/mv_av_fast_size
+        return (slow, fast)
+    return (-1, -1)
+
+
+'''
+
+
+logic() function:
+    Context: Called for every row in the input data.
+
+    Input:  account - the account object
+            lookback - the lookback dataframe, containing all data up until this point in time
+
+    Output: none, but the account object will be modified on each call
+'''
+
+closing = []
+slow = []
+fast = []
+x = []
+
+counter = 0
+
+
+def logic(account, lookback):  # Logic function to be used for each time interval in backtest
+    global counter
+    # how we can create a variable
+    try:
+        account.new_var += 1
+    except AttributeError:
+        account.new_var = 0
+
+    today = len(lookback)-1
+    # will only start returning non (-1, -1) when slow window has caught up
+    slow_av, fast_av = move_averages(lookback['close'][today])
+
+    closing.append(lookback['close'][today])
+    slow.append(slow_av)
+    fast.append(fast_av)
+    x.append(counter)
+    counter += 1
+
+
+'''
+preprocess_data() function:
+    Context: Called once at the beginning of the backtest. TOTALLY OPTIONAL. 
+             Each of these can be calculated at each time interval, however this is likely slower.
+
+    Input:  list_of_stocks - a list of stock data csvs to be processed
+
+    Output: list_of_stocks_processed - a list of processed stock data csvs
+'''
+
+
+def preprocess_data(list_of_stocks):
+    list_of_stocks_processed = []
+    for stock in list_of_stocks:
+        df = pd.read_csv("data/" + stock + ".csv", parse_dates=[0])
+        df['TP'] = (df['close'] + df['low'] + df['high']) / \
+            3  # Calculate Typical Price
+        # Calculate Standard Deviation
+        df['std'] = df['TP'].rolling(training_period).std()
+        # Calculate Moving Average of Typical Price
+        df['MA-TP'] = df['TP'].rolling(training_period).mean()
+        df['BOLU'] = df['MA-TP'] + standard_deviations * \
+            df['std']  # Calculate Upper Bollinger Band
+        df['BOLD'] = df['MA-TP'] - standard_deviations * \
+            df['std']  # Calculate Lower Bollinger Band
+        df.to_csv("data/" + stock + "_Processed.csv",
+                  index=False)  # Save to CSV
+        list_of_stocks_processed.append(stock + "_Processed")
+    return list_of_stocks_processed
+
+
+if __name__ == "__main__":
+    global closing, x
+    # list_of_stocks = ["TSLA_2020-03-01_2022-01-20_1min"]
+    # List of stock data csv's to be tested, located in "data/" folder
+    list_of_stocks = ["TSLA_2020-03-09_2022-01-28_15min",
+                      "AAPL_2020-03-24_2022-02-12_15min"]
+    list_of_stocks_proccessed = preprocess_data(
+        list_of_stocks)  # Preprocess the data
+    # Run backtest on list of stocks using the logic function
+    results = tester.test_array(list_of_stocks_proccessed, logic, chart=True)
+
+    print("training period " + str(training_period))
+    print("standard deviations " + str(standard_deviations))
+    df = pd.DataFrame(list(results), columns=["Buy and Hold", "Strategy", "Longs", "Sells",
+                      "Shorts", "Covers", "Stdev_Strategy", "Stdev_Hold", "Stock"])  # Create dataframe of results
+    df.to_csv("results/Test_Results.csv", index=False)  # Save results to csv
+
+    plt.plot(closing, x)
+    plt.show()
